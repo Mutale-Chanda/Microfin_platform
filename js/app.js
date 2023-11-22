f926ff94a2f54bb60c541caa49db84a06beeb72a
Mutale Chanda <mutalechanda016@gmail.com>
2023-11-22 08:15:46 +0200

    Initial project files

---
+++ js/app.js
@@ -0,0 +1,28 @@
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  autoplay: {
+    disableOnInteraction: false,
+    delay: 2000,
+  },
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+// mobile nav
+const hamburger = document.querySelector(".hamburger");
+const Nav = document.querySelector(".mobile_nav");
+
+hamburger.addEventListener("click", () => {
+  Nav.classList.toggle("mobile_nav_hide");
+});
